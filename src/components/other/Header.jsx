9e3ad806e2c78b07411a6ff61041096f2ac9fb72
Raj <rajchoudhary0311@gmail.com>
2026-03-14 23:32:19 +0530

    fourth day

--- src/components/other/Header.jsx
+++ src/components/other/Header.jsx
@@ -8,7 +8,7 @@ const Header = (props) => {
         HELLO 
       </h1>
 
-      <button className="bg-red-500 px-5 py-2 rounded-lg hover:bg-red-600 transition cursor-pointer">
+      <button >
         Logout
       </button>
 
